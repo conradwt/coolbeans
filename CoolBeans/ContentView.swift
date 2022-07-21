5cc512a7dc865be3d00bd6b47f21560a1f435df3
Conrad Taylor <conradwt@gmail.com>
2022-07-20 18:43:23 -0700

    Initial Commit

---
+++ CoolBeans/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoolBeans
+//
+//  Created by Conrad Taylor on 7/20/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CoolBeans/CoolBeansApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoolBeansApp.swift
+//  CoolBeans
+//
+//  Created by Conrad Taylor on 7/20/22.
+//
+
+import SwiftUI
+
+@main
+struct CoolBeansApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
